ff0d4cf59ce31410d5f046f34d6262a5b2fffc34
chanaka <chanakadrathnayaka@gmail.com>
2024-05-16 18:27:02 -0700

    Chart loading strategy is changed

--- src/app/components/realtime-chart/realtime-chart.component.ts
+++ src/app/components/realtime-chart/realtime-chart.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Component, inject, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {HighchartsChartModule} from "highcharts-angular";
 import {
   MatExpansionPanel,
@@ -10,7 +10,6 @@ import {SymbolService} from "../../services/symbol.service";
 import Highcharts from "highcharts";
 import * as Highstock from "highcharts/highstock";
 import {HighchartsData} from "../../models/HighchartData";
-import {TradeResponse} from "../../models/TradeResponse";
 
 @Component({
   selector: 'app-realtime-chart',
@@ -25,234 +24,175 @@ import {TradeResponse} from "../../models/TradeResponse";
   templateUrl: './realtime-chart.component.html',
   styleUrl: './realtime-chart.component.css'
 })
-export class RealtimeChartComponent implements OnInit, OnChanges, OnDestroy {
+export class RealtimeChartComponent implements OnChanges {
 
   @Input() symbol: string | null = null;
   symbolService: SymbolService = inject(SymbolService);
-  HighchartsInstance: typeof Highcharts = Highstock;
-  hasChartUpdated: boolean = false;
-  isLoading: boolean = true;
+  isLoading: boolean = false;
   error: string | null = null;
-  chartOptions: Highcharts.Options = {
-    title: {
-      style: {
-        color: '#efefef'
-      }
-    },
-    colors: ['#E91E63', '#3F51B5', '#F44336',
-      '#9C27B0', '#24CBE5', '#64E572', '#FF9655',
-      '#FFF263', '#6AF9C4'],
-    chart: {
-      backgroundColor: '#262626',
-    },
-    rangeSelector: {
-      buttons: [{
-        type: 'minute',
-        count: 1,
-        text: '1m'
-      }, {
-        type: 'minute',
-        count: 5,
-        text: '5m'
-      }, {
-        type: 'minute',
-        count: 15,
-        text: '15m'
-      }, {
-        type: 'minute',
-        count: 30,
-        text: '30m'
-      }, {
-        type: 'minute',
-        count: 60,
-        text: '60m'
-      }, {
-        type: 'all',
-        text: 'All'
-      }],
-      inputEnabled: false,
-      selected: 6
-    },
-    yAxis: [
-      {
-        labels: {
-          align: "left",
-          style: {
-            color: '#efefef'
-          }
-        },
-        height: "80%",
-        resize: {
-          enabled: true
-        }
-      },
-      {
-        labels: {
-          align: "left",
-          style: {
-            color: '#efefef'
-          }
-        },
-        top: "80%",
-        height: "20%",
-        offset: 0
-      }
-    ],
-    xAxis: [
-      {
-        labels: {
-          style: {
-            color: '#efefef'
-          }
-        },
-      },
-      {
-        labels: {
-          style: {
-            color: '#efefef'
-          }
-        },
-      }
-    ],
-    tooltip: {
-      headerShape: "callout",
-      borderWidth: 0,
-      shadow: false,
-      positioner: function (width, height, point) {
-        let chart = this.chart;
-        let position;
-        if (point.isHeader) {
-          position = {
-            x: Math.max(
-              // Left side limit
-              chart.plotLeft,
-              Math.min(
-                point.plotX + chart.plotLeft - width / 2,
-                // Right side limit
-                // @ts-ignore
-                chart.chartWidth - width - chart.marginRight
-              )
-            ),
-            y: point.plotY
-          };
-        } else {
-
-          position = {
-            x: point.series.chart.plotLeft,
-            // @ts-ignore
-            y: point.series.yAxis.top - chart.plotTop
-          };
-        }
-        return position;
-      }
-    },
-    series: [
-      {
-        type: "area",
-        data: []
-      },
-      {
-        type: "column",
-        data: [],
-      }
-    ],
-    responsive: {
-      rules: [
-        {
-          condition: {
-            maxWidth: 800
-          },
-          chartOptions: {
-            rangeSelector: {
-              inputEnabled: false
-            }
-          }
-        }
-      ]
-    }
-  };
   timerId: number = -1;
 
-  ngOnInit(): void {
-    this.timerId = setInterval(() => {
-      const dummy: TradeResponse = {
-        p: Math.random() * (200 - 160) + 160,
-        s: this.symbol!,
-        v: Math.random() * (100 - 1) + 1,
-        t: new Date().getTime()
-      }
-      this.addDataPoints(dummy);
-    }, 1000);
-  }
-
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes['symbol'].currentValue);
-    this.isLoading = true;
-
-    this.symbol && this.symbolService.getIntraday(this.symbol, '1min')
-    .subscribe({
-      next: (highChartData: HighchartsData) => {
-        this.isLoading = false;
-        this.initializeChart(highChartData);
-      },
-      error: err => {
-        this.isLoading = false;
-        this.error = err.error;
-      }
-    });
+    let changeSymbol = changes['symbol'];
+    if (changeSymbol !== null && changeSymbol.currentValue !== changeSymbol.previousValue) {
+      this.symbol && this.symbolService.getIntraday(this.symbol, '1min')
+      .subscribe({
+        next: (highChartData: HighchartsData) => {
+          this.isLoading = false;
+          this.setupChart(highChartData);
+        },
+        error: err => {
+          this.isLoading = false;
+          this.error = err.error;
+        }
+      });
+    }
   }
 
-  initializeChart(highChartData: HighchartsData): void {
-
-    this.chartOptions.title!.text = `${this.symbol} Stock Price`;
-    this.chartOptions.series = [
+  setupChart(highChartData: HighchartsData) {
+    const chart = new Highstock.StockChart(
       {
-        type: 'area',
-        id: `${this.symbol}-intraday`,
-        name: `${this.symbol} Stock Price`,
-        data: highChartData.ohlc,
-        gapSize: 5,
-        tooltip: {
-          valueDecimals: 2
+        chart: {
+          renderTo: 'container',
+          backgroundColor: '#484848',
+          events: {
+            load() {
+              const chart = this,
+                series = chart.series[0];
+// Imitate getting point from backend
+              // @ts-ignore
+              function getNewPoint(i, data) {
+                const lastPoint = data[data.length - 1];
+
+                // Add new point
+                if (i === 0 || i % 10 === 0) {
+                  return [
+                    lastPoint[0] + 60000,
+                    lastPoint[4],
+                    lastPoint[4],
+                    lastPoint[4],
+                    lastPoint[4]
+                  ];
+                }
+                const updatedLastPoint = data[data.length - 1],
+                  newClose = Highcharts.correctFloat(
+                    lastPoint[4] + Highcharts.correctFloat(Math.random() - 0.5, 2),
+                    4
+                  );
+
+                // Modify last data point
+                return [
+                  updatedLastPoint[0],
+                  data[data.length - 2][4],
+                  newClose >= updatedLastPoint[2] ? newClose : updatedLastPoint[2],
+                  newClose <= updatedLastPoint[3] ? newClose : updatedLastPoint[3],
+                  newClose
+                ];
+              }
+
+              let i = 0;
+
+              setInterval(() => {
+                if (series.options) { // @ts-ignore
+                  const data = series.options.data,
+                    newPoint = getNewPoint(i, data),
+                    lastPoint = data[data.length - 1];
+
+                  if (lastPoint[0] !== newPoint[0]) {
+                    series.addPoint(newPoint);
+                  } else {
+                    // @ts-ignore
+                    series.options.data[data.length - 1] = newPoint;
+
+                    series.setData(data);
+                  }
+                  i++;
+                }
+              }, 100);
+            }
+          }
         },
-        fillColor: {
-          linearGradient: {
-            x1: 0,
-            y1: 0,
-            x2: 0,
-            y2: 1
+
+        title: {
+          text: `${this.symbol} Stock Price`,
+          style: {
+            color: '#efefef'
+          }
+        },
+        colors: ['#E91E63', '#3F51B5', '#F44336',
+          '#9C27B0', '#24CBE5', '#64E572', '#FF9655',
+          '#FFF263', '#6AF9C4'],
+        xAxis: {
+          overscroll: 50000,
+          range: 4 * 200000,
+          gridLineWidth: 1,
+          labels: {
+            style: {
+              color: '#efefef'
+            }
+          }
+        },
+        yAxis: {
+          labels: {
+            style: {
+              color: '#efefef'
+            }
+          }
+        },
+
+        rangeSelector: {
+          buttons: [{
+            type: 'minute',
+            count: 1,
+            text: '1m'
+          }, {
+            type: 'minute',
+            count: 5,
+            text: '5m'
+          }, {
+            type: 'minute',
+            count: 15,
+            text: '15m'
+          }, {
+            type: 'minute',
+            count: 30,
+            text: '30m'
+          }, {
+            type: 'hour',
+            count: 1,
+            text: '1h'
+          }, {
+            type: 'all',
+            count: 1,
+            text: 'All'
+          }],
+          selected: 4, //1h
+          inputEnabled: false
+        },
+
+        navigator: {
+          series: {
+            color: '#000000'
+          }
+        },
+
+        series: [{
+          type: 'candlestick',
+          id: `${this.symbol}-price`,
+          name: `${this.symbol}`,
+          // color: '#FF7F7F',
+          // upColor: '#90EE90',
+          lastPrice: {
+            enabled: true,
+            label: {
+              enabled: true,
+              // backgroundColor: '#FF7F7F'
+            }
           },
-          stops: [
-            [0, '#E91E63'],
-            [1, '#E91E6300']
-          ]
-        },
-        threshold: null
-      },
-      {
-        type: "column",
-        id: `${this.symbol}-volume`,
-        name: `${this.symbol} Volume`,
-        data: highChartData.volume,
-        yAxis: 1
-      }
-    ];
-    this.hasChartUpdated = true;
-  }
+          data: highChartData.ohlc
+        }]
 
-  addDataPoints(data: TradeResponse) {
-    const priceDataPoint = [data.t, data.p];
-    const volumeDataPoint = [data.t, data.v];
 
-    console.log('Updating', priceDataPoint, volumeDataPoint)
-    // @ts-ignore
-    this.chartOptions.series[0].data.push(priceDataPoint);
-    // @ts-ignore
-    this.chartOptions.series[1].data.push(volumeDataPoint);
-    this.hasChartUpdated = true;
-  }
-
-  ngOnDestroy(): void {
-    clearInterval(this.timerId);
+      });
   }
 }
